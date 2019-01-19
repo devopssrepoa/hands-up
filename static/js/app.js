bc8b54b8b84e7321f40610276dd967b57b2929c8
Sebastian Webber <sebastian@swebber.me>
2019-01-19 21:13:17 -0200

    update layout and create "presentation" mode

--- static/js/app.js
+++ static/js/app.js
@@ -1,37 +1,47 @@
 new Vue({
     el: '#app',
-    data () {
-      return {
-        handList: null,
-        counter: 0
-      }
+    data() {
+        return {
+            handList: null,
+            counter: 0
+        }
     },
-    mounted () {
-      axios
-        .get('http://localhost:3000/api/hands')
-        .then(response => (this.handList = response.data.hands))
-        .catch(error => console.log(error))
+    mounted() {
+        axios
+            .get('http://localhost:3000/api/hands')
+            .then(response => (this.handList = response.data.hands))
+            .catch(error => console.log(error))
     },
     methods: {
         remove: function (event, who) {
-            var bodyFormData = new FormData();
-            bodyFormData.set('who', who);
-            axios({
-                method: 'post',
-                url: '/api/hands/drop',
-                data: bodyFormData,
-                config: { headers: {'Content-Type': 'application/x-www-form-urlencoded' }}
-                })
-              .then(function (response) {
-                console.log(response);
-              })
-              .catch(function (error) {
-                console.log(error);
-              });
-              var index = this.handList.indexOf(who);
-              if (index > -1) {
-                this.handList.splice(index, 1);
-              }
+
+            var vueStuff = this;
+
+            this.$dialog.confirm({
+                message: 'Delete from Queue?',
+                onConfirm: function () {
+                    var bodyFormData = new FormData();
+                    bodyFormData.set('who', who);
+                    axios({
+                        method: 'post',
+                        url: '/api/hands/drop',
+                        data: bodyFormData,
+                        config: { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } }
+                    })
+                        .then(function (response) {
+                            console.log(response);
+                        })
+                        .catch(function (error) {
+                            console.log(error);
+                        });
+
+
+                    var index = vueStuff.handList.indexOf(who);
+                    if (index > -1) {
+                        vueStuff.handList.splice(index, 1);
+                    }
+                }
+            });
         }
     }
 })
