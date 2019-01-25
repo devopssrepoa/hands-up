25007d51daaffdb2818a03195a440eeaca38ac11
Marco Antonio Martins Junior <somatorio@gmail.com>
2019-01-25 19:20:53 +0000

    Remove localstorage and applies sugestions from review

--- static/js/app.js
+++ static/js/app.js
@@ -3,51 +3,27 @@ new Vue({
     data() {
         return {
             handList: null,
-            who: '',
-            newWho: '',
+            who: ''
         }
     },
     methods: {
         requestRemove: function (event, who, confirm) {
-
-            var vueStuff = this;
-
             if (confirm == true) {
                 this.$dialog.confirm({
                     message: 'Remove from Queue?',
-                    onConfirm: () => this.remove(event, who, this)
+                    onConfirm: () => this.modify(event, 'drop', who, this)
                 });
-            } else {
-                this.remove(event, who, this);
+                return 
             }
+            this.modify(event, 'drop', who, this);
         },
-        remove: function (event, who, context) {
-            var vueStuff = context;
-        
-            var bodyFormData = new FormData();
-            bodyFormData.set('who', who);
-            axios({
-                method: 'post',
-                url: '/api/hands/drop',
-                data: bodyFormData,
-                config: { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } }
-            })
-                .then(function (response) {
-                    console.log(response);
-                })
-                .catch(function (error) {
-                    console.log(error);
-                });
-        },
-        add: function (event, who) {
-
-            var vueStuff = this;
+        modify: function (event, action, who, context) {
 
             var bodyFormData = new FormData();
             bodyFormData.set('who', who);
             axios({
                 method: 'post',
-                url: '/api/hands/rise',
+                url: `/api/hands/${action}`,
                 data: bodyFormData,
                 config: { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } }
             })
@@ -60,34 +36,24 @@ new Vue({
         },
         getHands: function (event) {
             axios
-            .get('/api/hands')
-            .then(response => (this.handList = response.data.hands))
-            .catch(error => console.log(error))
+                .get('/api/hands')
+                .then(response => (this.handList = response.data.hands))
+                .catch(error => console.log(error))
         },
         saveWho: function () {
-            if (this.newWho !== localStorage.who) {
-                if (this.handList !== null && this.handList.indexOf(this.newWho) != -1) {
-                    this.$toast.open({
-                        message: "You can't impersonate someone from the queue!",
-                        type: 'is-danger'
-                    })
-                } else {
-                    localStorage.who = this.newWho;
-                    if (this.handList !== null && this.handList.indexOf(this.who) != -1) {
-                        this.remove(event,this.who,this);
-                    }
-                    this.who = localStorage.who;
-                }
+            if (this.handList !== null && this.handList.indexOf(document.getElementById('who').value) != -1) {
+                this.$toast.open({
+                    message: "You can't impersonate someone from the queue!",
+                    type: 'is-danger'
+                })
+                return;
             }
+            
+            this.who = document.getElementById('who').value;
         }
     },
     mounted: function () { 
         this.getHands();
-        
-        if (localStorage.who) {
-            this.who = localStorage.who;
-            this.newWho = localStorage.who;
-          }
 
         setInterval(function () {
             this.getHands();
